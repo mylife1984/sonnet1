caf7c4511610a5fffd44ebcbdae2ce2ab997aef3
fangzx <fangzhouxing@gmail.com>
2010-09-04 08:47:04 +0800

    1

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
 <html>
 <head>
-   <META http-equiv=Content-Type content="text/html; charset=utf-8">
+<META http-equiv=Content-Type content="text/html; charset=utf-8">
 </head>
 <body>
 <?php
 echo "<p>Hello World 汉字</p>";
 
-echo __DIR__; 
-echo '<p/>';
-#echo realpath("../lithium");
-
-echo (dirname(__DIR__));
-
-?> 
+echo function_exists('readline')?'ok':'not ok';
+      
+?>
 </body>
 </html>
