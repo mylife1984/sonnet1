4606eec55201101d3b6cc3b6fef225b7809717de
fzx <fangzhouxing@gmail.com>
2010-10-06 22:11:35 +0800

    add 404/500 unit test!

--- apps/demo/route.js
+++ apps/demo/route.js
@@ -11,5 +11,6 @@ var server = module.parent.exports
 var service = require('./home'); 
 
 server.get('/', service.showIndex);
+server.get('/500page', service.show500page); //show500page不存在
 
 //EOP
--- tests/demo/test_home.js
+++ tests/demo/test_home.js
@@ -21,5 +21,41 @@ module.exports = {
 			assert.includes(res.body, 'My Express');
 		});
 	}
+	//
+	//expresso * -o 'can_show_404'
+	//
+	,'can_show_404' : function(assert) {
+		var req = {
+			url : '/notfoundpage',
+			method : 'GET'
+		}
+		var res = {
+			status : 200,
+			headers : {
+				'Content-Type' : 'text/html; charset=utf-8'
+			}
+		}
+		assert.response(server, req, res, function(res) {
+			assert.includes(res.body, 'Cannot find');
+		});
+	}
+	//
+	//expresso * -o 'can_show_500'
+	//
+	,'can_show_500' : function(assert) {
+		var req = {
+			url : '/500page',
+			method : 'GET'
+		}
+		var res = {
+			status : 200,
+			headers : {
+				'Content-Type' : 'text/html; charset=utf-8'
+			}
+		}
+		assert.response(server, req, res, function(res) {
+			assert.includes(res.body, '500 ERROR');
+		});
+	}
 };
 //EOP
