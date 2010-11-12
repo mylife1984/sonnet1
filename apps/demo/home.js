498c0d87c3732b92fd2df4049360a43ff686896c
fzx <fangzhouxing@gmail.com>
2010-11-12 21:00:32 +0800

    add BACKEND_URL.

--- apps/demo/home.js
+++ apps/demo/home.js
@@ -1,8 +1,9 @@
 /**
  * home
  */
-var rest = require('restler');
- 
+var rest = require('restler')
+    ,settings = require('settings')
+
 //显示主页 
 exports.showIndex = function(req, res){
     res.render('demo/index.html', {
@@ -19,7 +20,7 @@ exports.getJsonData = function(req,res) {
 
 //通过restler从其他系统获取json数据
 exports.getRemoteJsonData = function(req,res) {
-  rest.get('http://my.sh70119.com/erp/testonline').addListener('complete', function(data) {
+  rest.get(settings.BACKEND_URL+'/json').addListener('complete', function(data) {
       res.send(data);
   });  
 }
--- settings.js
+++ settings.js
@@ -3,4 +3,6 @@
  */
 exports.PATH_ROOT = __dirname;
 
+exports.BACKEND_URL = "http://localhost:3001"
+
 //EOP
